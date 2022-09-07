1ac1145da1f24a5a028f33f5664b61ba0c0d8d43
Denis19850204 <dg04021985@gmail.com>
2022-09-07 18:50:53 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
 
+let timeId = null;
+
+startBtn.addEventListener('click', clickOnStartBtn);
+stopBtn.addEventListener('click', clickOnStopBtn);
+console.log('privet');
+
+stopBtn.disabled = true;
+
+function clickOnStartBtn() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  timeId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function clickOnStopBtn() {
+  clearInterval(timeId);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
